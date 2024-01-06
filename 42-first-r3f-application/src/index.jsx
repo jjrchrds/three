a47f0dbb9300b4a5d1aa4f2edf6fd64002c9c8c8
Joshua Richards <joshua.j.richards@gmail.com>
2024-01-06 18:17:17 -0500

    test

--- 42-first-r3f-application/src/index.jsx
+++ 42-first-r3f-application/src/index.jsx
@@ -1,23 +1,23 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { XRButton, VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
 import Experience from './Experience'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
-    <VRButton />
-        <Canvas>
-            <XR>
-                <Controllers />
-                <Hands />
-                <mesh>
-                    <boxGeometry />
-                    <meshBasicMaterial color="blue" />
-                </mesh>
-            </XR>
-        </Canvas>
+    <XRButton />
+    <Canvas>
+        <XR>
+            <Controllers />
+            <Hands />
+            <mesh>
+                <boxGeometry />
+                <meshBasicMaterial color="blue" />
+            </mesh>
+        </XR>
+    </Canvas>
     </>
 )
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { XRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
 import Experience from './Experience'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
-    <VRButton />
+    <XRButton mode={"VR"} />
         <Canvas>
             <XR>
                 <Controllers />
