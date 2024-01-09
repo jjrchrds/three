312532fc663f9e00efad687e6459158d09c2fc36
Joshua Richards <joshua.j.richards@gmail.com>
2024-01-09 12:49:53 -0500

    test boxes

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,14 @@ import { Sky, Text } from '@react-three/drei'
 import '@react-three/fiber'
 import './style.css'
 import { Canvas } from '@react-three/fiber'
+import Box from './components/Box'
 import Button from './components/Button'
 import MovementController from './components/MovementController';
+import { OrbitControls } from '@react-three/drei'
 
-function Floor() {
+function Floor({...props}) {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+    <mesh rotation={[-Math.PI / 2, 0, 0]} {...props}>
       <planeGeometry args={[40, 40]} />
       <meshStandardMaterial color="#666" />
     </mesh>
@@ -18,22 +20,33 @@ function Floor() {
 }
 
 function App() {
+
+  const color = "red";
+
   return (
     <>
       <VRButton />
       <Canvas>
+        <OrbitControls />
         <XR>
           <Sky sunPosition={[0, 1, 0]} />
-          <Floor />
+          <Floor receiveShadow/>
           <ambientLight />
-          <pointLight position={[10, 10, 10]} />
+          <pointLight position={[10, 10, 10]} castShadow={true}/>
 
-          
-			<MovementController
-				hand="left"
-				applyRotation={false}
-				applyHorizontal={true}
-			/>
+          <Box
+            color={color}
+            // scale={hover ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+            size={[1, 1, 1]}
+            position={[0,1,0]}
+            castShadow
+          />
+          <MovementController applyForward={false}/>
+          <MovementController
+            hand="left"
+            applyRotation={false}
+            applyHorizontal={true}
+          />
 
           <Controllers />
           <Button position={[0, 0.8, -1]} />
