323129afc3a92c12186b7b2887c3d3e14163ff53
Joshua Richards <joshua.j.richards@gmail.com>
2024-01-09 22:34:12 -0500

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,50 +1,41 @@
-import React, { useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Interactive, XR, Controllers, VRButton } from '@react-three/xr'
-import { Sky, Text } from '@react-three/drei'
-import '@react-three/fiber'
-import './style.css'
-import { Canvas } from '@react-three/fiber'
-import Box from './components/Box'
-import Button from './components/Button'
-import MovementController from './components/MovementController';
-import { OrbitControls } from '@react-three/drei'
-import Experience from './components/Experience'
-
-
-import Perampanel from './components/Perampanel'
-
-
-
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { Interactive, XR, Controllers, VRButton } from "@react-three/xr";
+import { Sky, Text } from "@react-three/drei";
+import "@react-three/fiber";
+import "./style.css";
+import { Canvas } from "@react-three/fiber";
+import Box from "./components/Box";
+import Button from "./components/Button";
+import MovementController from "./components/MovementController";
+import { OrbitControls } from "@react-three/drei";
+import Experience from "./components/Experience";
 
 function App() {
-
   const color = "red";
-
 
   return (
     <>
       <VRButton />
-      <Canvas>
+      <Canvas camera={{ position: [2, 4, 8] }} shadows>
         <OrbitControls />
         <XR>
           <Experience />
 
-
-          <MovementController applyForward={false}/>
+          <MovementController applyForward={false} />
           <MovementController
             hand="left"
             applyRotation={false}
             applyHorizontal={true}
           />
-    
+
           <Controllers />
         </XR>
       </Canvas>
     </>
-  )
+  );
 }
 
 //@ts-ignore
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
